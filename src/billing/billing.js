a434bc0bdb4e42251aac13579dd25009221d0ec8
Marcos Siqueira Junior <marcosjunior182@gmail.com>
2019-10-27 16:43:11 -0400

    fix: missing billing logic methods

--- src/billing/billing.js
+++ src/billing/billing.js
@@ -1,4 +1,6 @@
+const taxes = require('../taxes/taxes');
 
+const EMPTY_TOTAL = { total: 0, taxes: 0 };
 
 function _totalString({ taxes, total }) {
   return `Sales Taxes: ${taxes.toFixed(2)}\nTotal: ${total.toFixed(2)}`
@@ -10,6 +12,18 @@ function _productString({ qty, name, price }) {
 
 function _itemsString(items) {
   return items.reduce((str, item) => (str + _productString(item) + '\n'), '');
+}
+
+function _calculateTotal(items) {
+  return items.reduce((value, item) => {
+    const productTotal = item.price * item.qty;
+    const productTaxes = item.qty * taxes.getTaxes(item);
+
+    return ({
+      total: +(value.total + productTaxes + productTotal).toFixed(2),
+      taxes: +(value.taxes + productTaxes).toFixed(2)
+    });
+  }, EMPTY_TOTAL);
 }
 
 function getReceipt(filePath) {
@@ -24,5 +38,6 @@ module.exports = {
   getReceipt,
   _totalString,
   _productString,
-  _itemsString
+  _itemsString,
+  _calculateTotal
 }
--- src/billing/billing.test.js
+++ src/billing/billing.test.js
@@ -1,20 +1,20 @@
 const billing = require('./billing');
 
-function mockProduct(qty, name, price) {
+function mockProduct(qty, name, category, price) {
   return ({
     qty: qty,
     name: name,
-    category: 'other',
+    category: category,
     price: price
   });
 };
 
 describe('Billing', () => {
   it('should convert product to string', () => {
-    const oneApple = mockProduct(1, 'apple', 2.2);
+    const oneApple = mockProduct(1, 'apple', 'food', 2.2);
     expect(billing._productString(oneApple)).toBe('1 apple: 2.20');
 
-    const threeApples = mockProduct(3, 'apple', 2.2);
+    const threeApples = mockProduct(3, 'apple', 'food', 2.2);
     expect(billing._productString(threeApples)).toBe('3 apple: 6.60');
   });
 
@@ -25,12 +25,22 @@ describe('Billing', () => {
 
   it('should convert a list of products to string', () => {
     const products = [
-      mockProduct(2, 'apple', 2.2),
-      mockProduct(1, 'pineapple', 5.26)
+      mockProduct(2, 'apple', 'food', 2.2),
+      mockProduct(1, 'pineapple', 'food', 5.26)
     ];
 
     expect(billing._itemsString(products))
       .toBe('2 apple: 4.40\n1 pineapple: 5.26\n');
+  });
+
+  it('should calculate the totals correctly', () => {
+    const products = [
+      mockProduct(2, 'apple', 'food', 2.2),
+      mockProduct(3, 'pineapple', 'other', 10.00)
+    ];
+
+    expect(billing._calculateTotal(products))
+      .toEqual({ total: 34.43, taxes: 0.03 });
   });
 
 })
